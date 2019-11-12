8f2b07875c47d35149fa6d20b9c2285d67620849
RexMa <Rex.magana@gmail.com>
2019-11-12 07:20:54 -0800

    Speed improvements and added more config
    The widget now uses animatedBuilders, responds to rebuilds with new parameters and adds two fields: curve and isAnimating. The curve is for the alpha and the isAnimating is a flag to animate the glow or not.

--- lib/avatar_glow.dart
+++ lib/avatar_glow.dart
@@ -5,23 +5,27 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class AvatarGlow extends StatefulWidget {
-  final bool repeat;
-  final Duration duration;
-  final double endRadius;
-  final Duration repeatPauseDuration;
   final Widget child;
+  final double endRadius;
+  final BoxShape shape;
+  final Duration duration;
+  final bool repeat;
+  final bool isAnimating;
+  final Duration repeatPauseDuration;
+  final Curve curve;
   final bool showTwoGlows;
   final Color glowColor;
   final Duration startDelay;
-  final BoxShape shape;
 
-  AvatarGlow({
-    @required this.endRadius,
+  const AvatarGlow({
     @required this.child,
+    @required this.endRadius,
     this.shape,
     this.duration,
     this.repeat = true,
+    this.isAnimating = true,
     this.repeatPauseDuration,
+    this.curve = Curves.fastOutSlowIn,
     this.showTwoGlows = true,
     this.glowColor,
     this.startDelay,
@@ -37,41 +41,76 @@ class _AvatarGlowState extends State<AvatarGlow>
   Animation<double> bigDiscAnimation;
   Animation<double> alphaAnimation;
   AnimationController controller;
+  void Function(AnimationStatus status) listener;
 
   @override
   void initState() {
-    super.initState();
     controller = AnimationController(
-        duration: widget.duration ?? Duration(milliseconds: 2000), vsync: this);
-    final Animation curve =
-        CurvedAnimation(parent: controller, curve: Curves.decelerate);
+      duration: widget.duration ?? Duration(milliseconds: 2000),
+      vsync: this,
+    );
+
+    if (widget.isAnimating) {
+      _createAnimation();
+      _startAnimation();
+    }
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(AvatarGlow oldWidget) {
+    _createAnimation();
+
+    if (widget.isAnimating) {
+      _startAnimation();
+    } else {
+      _stopAnimation();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _createAnimation() {
+    final Animation curve = CurvedAnimation(
+      parent: controller,
+      curve: widget.curve,
+    );
+
     smallDiscAnimation = Tween(
-            begin: (widget.endRadius * 2) / 6,
-            end: (widget.endRadius * 2) * (3 / 4))
-        .animate(curve)
-          ..addListener(() {
-            setState(() {});
-          });
-    bigDiscAnimation =
-        Tween(begin: 0.0, end: (widget.endRadius * 2)).animate(curve)
-          ..addListener(() {
-            setState(() {});
-          });
+      begin: (widget.endRadius * 2) / 6,
+      end: (widget.endRadius * 2) * (3 / 4),
+    ).animate(curve);
+
+    bigDiscAnimation = Tween(
+      begin: 0.0,
+      end: (widget.endRadius * 2),
+    ).animate(curve);
+
     alphaAnimation = Tween(begin: 0.30, end: 0.0).animate(controller);
-    controller.addStatusListener((_) async {
+
+    controller.removeStatusListener(listener);
+
+    listener = (_) async {
       if (controller.status == AnimationStatus.completed) {
         await Future.delayed(
             widget.repeatPauseDuration ?? Duration(milliseconds: 100));
+
         if (mounted && widget.repeat) {
           controller.reset();
           controller.forward();
         }
       }
-    });
-    startAnimation();
+    };
+
+    controller.addStatusListener(listener);
   }
 
-  void startAnimation() async {
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() async {
     if (widget.startDelay != null) {
       await Future.delayed(widget.startDelay);
       if (mounted) controller.forward();
@@ -80,48 +119,59 @@ class _AvatarGlowState extends State<AvatarGlow>
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: widget.endRadius * 2,
-      width: widget.endRadius * 2,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(
-            height: bigDiscAnimation.value,
-            width: bigDiscAnimation.value,
-            child: SizedBox(),
-            decoration: BoxDecoration(
-              shape: widget.shape ?? BoxShape.circle,
-              color: (widget.glowColor ?? Colors.white)
-                  .withOpacity(alphaAnimation.value),
-            ),
-          ),
-          widget.showTwoGlows
-              ? Container(
-                  height: smallDiscAnimation.value,
-                  width: smallDiscAnimation.value,
-                  child: SizedBox(),
-                  decoration: BoxDecoration(
-                    shape: widget.shape ?? BoxShape.circle,
-                    color: (widget.glowColor ?? Colors.white)
-                        .withOpacity(alphaAnimation.value),
-                  ),
-                )
-              : SizedBox(
-                  height: 0.0,
-                  width: 0.0,
-                ),
-          widget.child,
-        ],
-      ),
-    );
+  void _stopAnimation() async {
+    controller?.reset();
   }
 
   @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    // AnimatedBuilder creates its own closure for some reason
+    // and widget.shape is not available inside its builder method
+    final shape = widget.shape;
+    final glowColor = widget.glowColor;
+
+    return AnimatedBuilder(
+      animation: alphaAnimation,
+      child: widget.child,
+      builder: (context, widgetChild) {
+        final decoration = BoxDecoration(
+          shape: shape ?? BoxShape.circle,
+          color: (glowColor ?? Colors.white).withOpacity(alphaAnimation.value),
+        );
+
+        return Container(
+          height: widget.endRadius * 2,
+          width: widget.endRadius * 2,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              AnimatedBuilder(
+                animation: bigDiscAnimation,
+                builder: (context, widget) {
+                  return Container(
+                    height: bigDiscAnimation.value,
+                    width: bigDiscAnimation.value,
+                    decoration: decoration,
+                  );
+                },
+              ),
+              widget.showTwoGlows
+                  ? AnimatedBuilder(
+                      animation: smallDiscAnimation,
+                      builder: (context, widget) {
+                        return Container(
+                          height: smallDiscAnimation.value,
+                          width: smallDiscAnimation.value,
+                          decoration: decoration,
+                        );
+                      },
+                    )
+                  : const SizedBox(height: 0.0, width: 0.0),
+              widgetChild,
+            ],
+          ),
+        );
+      },
+    );
   }
 }
