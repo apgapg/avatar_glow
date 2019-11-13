07925e9534a9c3977f25e7c423a4b42424ec5974
Ayush P Gupta <ayushpguptaapg@gmail.com>
2019-11-13 08:44:50 +0530

    Add macos support

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
 import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+// Sets a platform override for desktop to avoid exceptions. See
+// https://flutter.dev/desktop#target-platform-override for more info.
+void _enablePlatformOverrideForDesktop() {
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
+
+void main() {
+  _enablePlatformOverrideForDesktop();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
