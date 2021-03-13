404633cdf0f4a093cdca42bcdbb74bb962cc77ec
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-03-13 18:39:35 +0530

    Update example code
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,7 +36,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double _radius = 70;
+  bool _animate = true;
 
   @override
   Widget build(BuildContext context) {
@@ -45,109 +45,14 @@ class _HomePageState extends State<HomePage> {
         title: Text("Avatar Glow by @apgapg"),
       ),
       body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                color: Color(0xff686de0),
-                child: AvatarGlow(
-                  endRadius: _radius,
-                  child: Material(
-                    elevation: 8.0,
-                    shape: CircleBorder(),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.grey[100],
-                      child: Image.asset(
-                        'assets/images/dart.png',
-                        height: 50,
-                      ),
-                      radius: 30,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                color: Color(0xff686de0),
-                child: AvatarGlow(
-                  startDelay: Duration(milliseconds: 1000),
-                  glowColor: Colors.white,
-                  endRadius: 100.0,
-                  duration: Duration(milliseconds: 2000),
-                  repeat: true,
-                  showTwoGlows: true,
-                  repeatPauseDuration: Duration(milliseconds: 100),
-                  child: Material(
-                    elevation: 8.0,
-                    shape: CircleBorder(),
-                    color: Colors.transparent,
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/avatar.png'),
-                      radius: 50.0,
-                    ),
-                  ),
-                  shape: BoxShape.circle,
-                  animate: true,
-                  curve: Curves.fastOutSlowIn,
-                ),
-              ),
-              SizedBox(
-                height: 16.0,
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  AvatarGlow(
-                    glowColor: Colors.blue,
-                    endRadius: 90.0,
-                    duration: Duration(milliseconds: 2000),
-                    repeat: true,
-                    showTwoGlows: true,
-                    repeatPauseDuration: Duration(milliseconds: 100),
-                    child: Material(
-                      elevation: 8.0,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[100],
-                        child: Image.asset(
-                          'assets/images/flutter.png',
-                          height: 60,
-                        ),
-                        radius: 40.0,
-                      ),
-                    ),
-                  ),
-                  AvatarGlow(
-                    glowColor: Colors.red,
-                    endRadius: 90.0,
-                    duration: Duration(milliseconds: 2000),
-                    repeat: true,
-                    showTwoGlows: true,
-                    repeatPauseDuration: Duration(milliseconds: 100),
-                    child: Material(
-                      elevation: 8.0,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey[100],
-                        child: Image.asset(
-                          'assets/images/love.png',
-                          height: 60,
-                        ),
-                        radius: 40.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              AvatarGlow(
-                glowColor: Colors.cyan,
-                endRadius: 60.0,
-                duration: Duration(milliseconds: 2000),
-                repeat: true,
-                showTwoGlows: true,
-                repeatPauseDuration: Duration(milliseconds: 100),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              color: Color(0xff686de0),
+              child: AvatarGlow(
+                endRadius: 70,
+                animate: _animate,
                 child: Material(
                   elevation: 8.0,
                   shape: CircleBorder(),
@@ -157,18 +62,112 @@ class _HomePageState extends State<HomePage> {
                       'assets/images/dart.png',
                       height: 50,
                     ),
-                    radius: 30.0,
+                    radius: 30,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              width: double.infinity,
+              color: Color(0xff686de0),
+              child: AvatarGlow(
+                startDelay: Duration(milliseconds: 1000),
+                glowColor: Colors.white,
+                endRadius: 100.0,
+                duration: Duration(milliseconds: 2000),
+                repeat: true,
+                showTwoGlows: true,
+                repeatPauseDuration: Duration(milliseconds: 100),
+                child: Material(
+                  elevation: 8.0,
+                  shape: CircleBorder(),
+                  color: Colors.transparent,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/avatar.png'),
+                    radius: 50.0,
+                  ),
+                ),
+                shape: BoxShape.circle,
+                animate: true,
+                curve: Curves.fastOutSlowIn,
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                AvatarGlow(
+                  glowColor: Colors.blue,
+                  endRadius: 90.0,
+                  duration: Duration(milliseconds: 2000),
+                  repeat: true,
+                  showTwoGlows: true,
+                  repeatPauseDuration: Duration(milliseconds: 100),
+                  child: Material(
+                    elevation: 8.0,
+                    shape: CircleBorder(),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey[100],
+                      child: Image.asset(
+                        'assets/images/flutter.png',
+                        height: 60,
+                      ),
+                      radius: 40.0,
+                    ),
+                  ),
+                ),
+                AvatarGlow(
+                  glowColor: Colors.red,
+                  endRadius: 90.0,
+                  duration: Duration(milliseconds: 2000),
+                  repeat: true,
+                  showTwoGlows: true,
+                  repeatPauseDuration: Duration(milliseconds: 100),
+                  child: Material(
+                    elevation: 8.0,
+                    shape: CircleBorder(),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey[100],
+                      child: Image.asset(
+                        'assets/images/love.png',
+                        height: 60,
+                      ),
+                      radius: 40.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            AvatarGlow(
+              glowColor: Colors.cyan,
+              endRadius: 60.0,
+              duration: Duration(milliseconds: 2000),
+              repeat: true,
+              showTwoGlows: true,
+              repeatPauseDuration: Duration(milliseconds: 100),
+              child: Material(
+                elevation: 8.0,
+                shape: CircleBorder(),
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey[100],
+                  child: Image.asset(
+                    'assets/images/dart.png',
+                    height: 50,
+                  ),
+                  radius: 30.0,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            _radius = 150;
+            _animate = !_animate;
           });
         },
       ),
