ff7a3c803cec0e94efd8d1e0d270cc7029819306
Sahil Kumar <xdsahil@gmail.com>
2023-07-14 09:04:20 +0530

    refactor!: v3.0.0 (#34)
    * refactor!: v3.0.0
    
    Signed-off-by: xsahil03x <xdsahil@gmail.com>
    
    * chore: apply gitignore
    
    Signed-off-by: xsahil03x <xdsahil@gmail.com>
    
    * chore: add screenshot
    
    Signed-off-by: xsahil03x <xdsahil@gmail.com>
    
    ---------
    
    Signed-off-by: xsahil03x <xdsahil@gmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,25 +2,27 @@ import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Avatar Glow Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
+      theme: ThemeData(useMaterial3: true),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -30,137 +32,128 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Avatar Glow by @apgapg"),
+        title: const Text("Avatar Glow by @apgapg"),
       ),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              color: Color(0xff686de0),
-              child: AvatarGlow(
-                endRadius: 70,
-                animate: _animate,
-                child: Material(
-                  elevation: 8.0,
-                  shape: CircleBorder(),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.grey[100],
-                    child: Image.asset(
-                      'assets/images/dart.png',
-                      height: 50,
-                    ),
-                    radius: 30,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              color: Color(0xff686de0),
-              child: AvatarGlow(
-                startDelay: Duration(milliseconds: 1000),
-                glowColor: Colors.white,
-                endRadius: 100.0,
-                duration: Duration(milliseconds: 2000),
-                repeat: true,
-                showTwoGlows: true,
-                repeatPauseDuration: Duration(milliseconds: 100),
-                child: Material(
-                  elevation: 8.0,
-                  shape: CircleBorder(),
-                  color: Colors.transparent,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/images/avatar.png'),
-                    radius: 50.0,
-                  ),
-                ),
-                shape: BoxShape.circle,
-                animate: true,
-                curve: Curves.fastOutSlowIn,
-              ),
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                    width: 100,
-                    height: 100,
-                    child: AvatarGlow(
-                      glowColor: Colors.red,
-                      endRadius: 90.0,
-                      duration: Duration(milliseconds: 2000),
-                      repeat: true,
-                      showTwoGlows: true,
-                      repeatPauseDuration: Duration(milliseconds: 100),
-                      child: Material(
-                        elevation: 8.0,
-                        shape: CircleBorder(),
-                        child: CircleAvatar(
-                          backgroundColor: Colors.grey[100],
-                          child: Image.asset(
-                            'assets/images/flutter.png',
-                            height: 60,
-                          ),
-                          radius: 40.0,
-                        ),
-                      ),
-                    )),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(32.0),
+            color: Theme.of(context).primaryColor,
+            child: Column(
+              children: [
                 AvatarGlow(
-                  glowColor: Colors.red,
-                  endRadius: 90.0,
-                  duration: Duration(milliseconds: 2000),
-                  repeat: true,
-                  showTwoGlows: true,
-                  repeatPauseDuration: Duration(milliseconds: 100),
+                  animate: _animate,
                   child: Material(
                     elevation: 8.0,
-                    shape: CircleBorder(),
+                    shape: const CircleBorder(),
                     child: CircleAvatar(
                       backgroundColor: Colors.grey[100],
+                      radius: 30,
                       child: Image.asset(
-                        'assets/images/love.png',
-                        height: 60,
+                        'assets/images/dart.png',
+                        height: 50,
                       ),
-                      radius: 40.0,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32.0),
+                AvatarGlow(
+                  startDelay: const Duration(milliseconds: 1000),
+                  glowColor: Colors.white,
+                  glowShape: BoxShape.circle,
+                  animate: _animate,
+                  curve: Curves.fastOutSlowIn,
+                  child: const Material(
+                    elevation: 8.0,
+                    shape: CircleBorder(),
+                    color: Colors.transparent,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/avatar.png'),
+                      radius: 50.0,
                     ),
                   ),
                 ),
               ],
             ),
-            AvatarGlow(
-              glowColor: Colors.cyan,
-              endRadius: 60.0,
-              duration: Duration(milliseconds: 2000),
-              repeat: true,
-              showTwoGlows: true,
-              repeatPauseDuration: Duration(milliseconds: 100),
-              child: Material(
-                elevation: 8.0,
-                shape: CircleBorder(),
-                child: CircleAvatar(
-                  backgroundColor: Colors.grey[100],
-                  child: Image.asset(
-                    'assets/images/dart.png',
-                    height: 50,
+          ),
+          const SizedBox(height: 32.0),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              AvatarGlow(
+                animate: _animate,
+                glowColor: Colors.red,
+                child: Material(
+                  elevation: 8.0,
+                  shape: const CircleBorder(),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey[100],
+                    radius: 40.0,
+                    child: Image.asset(
+                      'assets/images/flutter.png',
+                      height: 60,
+                    ),
                   ),
-                  radius: 30.0,
+                ),
+              ),
+              AvatarGlow(
+                animate: _animate,
+                glowColor: Colors.red,
+                child: Material(
+                  elevation: 8.0,
+                  shape: const CircleBorder(),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey[100],
+                    radius: 40.0,
+                    child: Image.asset(
+                      'assets/images/love.png',
+                      height: 60,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 32.0),
+          AvatarGlow(
+            animate: _animate,
+            glowColor: Colors.cyan,
+            child: Material(
+              elevation: 8.0,
+              shape: const CircleBorder(),
+              child: CircleAvatar(
+                backgroundColor: Colors.grey[100],
+                radius: 30.0,
+                child: Image.asset(
+                  'assets/images/dart.png',
+                  height: 50,
                 ),
               ),
             ),
-          ],
+          ),
+        ],
+      ),
+      floatingActionButton: AvatarGlow(
+        animate: _animate,
+        glowColor: Colors.red,
+        child: IconButton(
+          onPressed: () {
+            setState(() => _animate = !_animate);
+          },
+          iconSize: 42.0,
+          icon: Icon(
+            _animate ? Icons.pause_rounded : Icons.play_arrow_rounded,
+            color: Colors.white,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _animate = !_animate;
-          });
-        },
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8,
+        clipBehavior: Clip.antiAlias,
+        color: Theme.of(context).primaryColor,
+        shape: const CircularNotchedRectangle(),
       ),
     );
   }
